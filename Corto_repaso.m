72e75190254b89679e55e750d978544ad64ba88c
Diego Terraza <93099927+Diegotrz@users.noreply.github.com>
2023-07-28 12:59:53 -0600

    Comenzando el problema 4

--- Corto_repaso.m
+++ Corto_repaso.m
@@ -4,9 +4,9 @@ clear
 TBA = [-4 -3 -1];
 TBC= [-4 1 -4];
 TBD = [-4 2 4];
-lmTBA = TBA/norm(TBA)
-lmTBC = TBC/norm(TBC)
-lmTBD = TBD/norm(TBD)
+lmTBA = TBA/norm(TBA);
+lmTBC = TBC/norm(TBC);
+lmTBD = TBD/norm(TBD);
 
 %Sumatoria de fuerzas
 sumfx= [lmTBA(1) lmTBC(1) lmTBD(1)];
@@ -15,8 +15,11 @@ sumfy= [lmTBA(2) lmTBC(2) lmTBD(2)];
 sumfz= [lmTBA(3) lmTBC(3) lmTBD(3)];
 
 %Matriz general
-A= [sumfx;sumfy;sumfz]
+A= [sumfx;sumfy;sumfz];
 inversaA= inv(A);
 x= [0;200*9.8;0];
 format shortG 
 Rp= inversaA*x;
+
+%Ejercicio 4
+MatrizF= [-0.099 -0.518 -0.85]
